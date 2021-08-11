792bc0a0754e29ce1b029f4caef83a592b6d3c3a
Mohammad Amaan <mdamaan00@gmail.com>
2021-08-11 15:45:28 +0530

    version 2

--- ytapi/views.py
+++ ytapi/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
-from knox.models import AuthToken
+
 from rest_framework.decorators import api_view
 from rest_framework.serializers import Serializer
 from .serializers import SongSerializer, UserSerializer, RegisterSerializer
@@ -8,32 +8,8 @@ from .models import *
 from django.contrib.auth import login
 
 from rest_framework.authtoken.serializers import AuthTokenSerializer
-from knox.views import LoginView as KnoxLoginView
 
 
-# Register API
-class RegisterAPI(generics.GenericAPIView):
-    serializer_class = RegisterSerializer
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        return Response({
-        "user": UserSerializer(user, context=self.get_serializer_context()).data,
-        "token": AuthToken.objects.create(user)[1]
-        })
-
-
-class LoginAPI(KnoxLoginView):
-    permission_classes = (permissions.AllowAny,)
-
-    def post(self, request, format=None):
-        serializer = AuthTokenSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        login(request, user)
-        return super(LoginAPI, self).post(request, format=None)
 
 
     
